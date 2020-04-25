8d31768876248df6cce8dc7b7a4675320de9b1f6
Alfheim <281508958@qq.com>
2020-04-25 19:11:30 +0800

    second_commit

---
+++ src/fork_test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unistd.h> 
+
+using namespace std;
+
+void fork_test_1(){
+	for(int i = 0 ; i < 2 ; ++i){
+		int pid = fork();
+		if(pid == 0){
+			cout<<"I am child: "<<getpid()<<endl;
+		}else{
+			cout<<"I am father: "<<getpid()<<endl;
+		}	
+	}
+}
+
+void fork_test_2(){
+      	for(int i = 0 ; i < 2 ; ++i){
+                int pid = fork();
+                if(pid == 0){
+                        cout<<"I am child: "<<getpid()<<endl;
+			break;
+                }else{
+                        cout<<"I am father: "<<getpid()<<endl;
+                }
+        }
+}
+
+void fork_test_3(){
+        for(int i = 0 ; i < 2 ; ++i){
+                int pid = fork();
+                cout<<"-\n";
+        }
+}
+
+void fork_test_4(){
+        for(int i = 0 ; i < 2 ; ++i){
+                int pid = fork();
+                cout<<"-";
+        }
+}
+
+
+int main(){
+	fork_test_4();
+	return 0;
+}
